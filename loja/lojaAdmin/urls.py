6b1cbec5cba7fc0aced5b239f66f803416cdb147
Maria Eduarda do Nascimento Silva <20221011110004@ifrn.local>
2024-11-08 11:32:35 -0300

    atualizado

--- loja/lojaAdmin/urls.py
+++ loja/lojaAdmin/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loja.urls.HomeUrls')),
     path('produto/', include('loja.urls.ProdutoUrls')),
-    path('usuario/', include('loja.urls.UsuarioUrls'))
+    path('usuario/', include('loja.urls.UsuarioUrls')),
+    path('', include('loja.urls.AuthUrls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
