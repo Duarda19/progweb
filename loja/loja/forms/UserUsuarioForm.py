690e48527e46dbb75668e51e10c5b10292050af7
Maria Eduarda do Nascimento Silva <20221011110004@ifrn.local>
2024-11-08 11:18:38 -0300

    cap9

--- loja/loja/forms/UserUsuarioForm.py
+++ loja/loja/forms/UserUsuarioForm.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from loja.models.Usuario import Usuario
+from django.contrib.auth.models import User
 class UserUsuarioForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(UserUsuarioForm, self).__init__(*args, **kwargs)
@@ -16,3 +17,14 @@ class UserUsuarioForm(ModelForm):
         # Usamos exclude para excluir campos específicos do sistema
         # exclude = ['']
         widgets = {'user': forms.HiddenInput(), 'perfil': forms.Select(attrs={'class': "form-control"}), 'aniversario': forms.DateInput(attrs={'class': "form-control", "type": "date"})}
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+        'username': forms.TextInput(attrs={'class': "form-control"}),
+        'email': forms.EmailInput(attrs={'class': "form-control"}),
+        'first_name': forms.TextInput(attrs={'class': "form-control"}),
+        'last_name': forms.TextInput(attrs={'class': "form-control"})
+        }
--- loja/loja/views/UsuarioView.py
+++ loja/loja/views/UsuarioView.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
-from loja.forms.UserUsuarioForm import UserUsuarioForm
+from loja.forms.UserUsuarioForm import *
 def list_usuario_view(request, id=None):
     usuarios = Usuario.objects.filter(perfil=2)
     context = {
@@ -9,8 +9,30 @@ def list_usuario_view(request, id=None):
     return render(request, template_name='usuario/usuario.html', context=context,status=200)
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
-    usuarioForm = UserUsuarioForm(instance=usuario)
+    emailUnused = True
+    message = None
+    if request.method == 'POST':
+        usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
+        userForm = UserForm(request.POST, instance=request.user)
+        verifyEmail =Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
+    if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+        usuarioForm.save()
+        userForm.save()
+        message = { 'type': 'success', 'text': 'Dados atualizados com sucesso' }
+    else:
+        if request.method == 'POST':
+            if emailUnused:
+                message = { 'type': 'danger', 'text': 'Dados inválidos' }
+            else:
+                message = { 'type': 'warning', 'text': 'E-mail já usado' }
     context = {
-        'usuarioForm': usuarioForm
+        'usuarioForm': usuarioForm,
+        'userForm': userForm,
+        'message': message
     }
+
     return render(request, template_name='usuario/usuario-edit.html', context=context,status=200) 
